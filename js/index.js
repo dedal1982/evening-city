ea9f3e2b420ed2ec73de0edf9b43a6200eed4af2
aldm <d44.a1@yandex.ru>
2024-04-08 05:03:43 +0300

    1.7

--- js/index.js
+++ js/index.js
@@ -28,24 +28,3 @@ burgerClick.addEventListener("click", () => {
 });
 
 //новости
-const wrapper = document.querySelector(".tabs");
-const tabToggle = wrapper.querySelectorAll(".tab-toggle");
-
-function openTab() {
-  const content = this.parentElement.nextElementSibling;
-  const activeItems = wrapper.querySelectorAll(".active");
-  if (window.innerWidth > 991) {
-    if (!this.classList.contains("active")) {
-      this.classList.toggle("active");
-      content.classList.toggle("active");
-      activeItems.forEach((item) => item.classList.toggle("active"));
-      wrapper.style.minHeight = `${content.offsetHeight}px`;
-    }
-  } else {
-    this.classList.toggle("active");
-    content.classList.toggle("active");
-  }
-}
-
-tabToggle.forEach((item) => item.addEventListener("click", openTab));
-tabToggle[0].click();
