b6f650849a3923f6765c5c61b33a65d63c527b50
aldm <d44.a1@yandex.ru>
2024-04-08 04:26:32 +0300

    1.5

--- js/index.js
+++ js/index.js
@@ -26,3 +26,26 @@ burgerClick.addEventListener("click", () => {
   navMobile.classList.toggle("active");
   scrollLock.classList.toggle("lock");
 });
+
+//новости
+const wrapper = document.querySelector(".tabs");
+const tabToggle = wrapper.querySelectorAll(".tab-toggle");
+
+function openTab() {
+  const content = this.parentElement.nextElementSibling;
+  const activeItems = wrapper.querySelectorAll(".active");
+  if (window.innerWidth > 991) {
+    if (!this.classList.contains("active")) {
+      this.classList.toggle("active");
+      content.classList.toggle("active");
+      activeItems.forEach((item) => item.classList.toggle("active"));
+      wrapper.style.minHeight = `${content.offsetHeight}px`;
+    }
+  } else {
+    this.classList.toggle("active");
+    content.classList.toggle("active");
+  }
+}
+
+tabToggle.forEach((item) => item.addEventListener("click", openTab));
+tabToggle[0].click();
